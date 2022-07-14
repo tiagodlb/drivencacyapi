f28802d6de6e03897ac4dc20936aea1f9cf1115e
Tiago <tigolimab223@gmail.com>
2022-07-14 18:14:21 -0300

    bug fix

--- middlewares/validateChoice.js
+++ middlewares/validateChoice.js
@@ -4,7 +4,7 @@ export function validateChoice(req, res, next) {
   const { error } = choiceSchema.validate(req.body, { abortEarly: false });
   if (error) {
     console.log(error);
-    return res.sendStatus(422);
+    return res.status(422).send(error.details.map((detail) => detail.message));
   }
   next();
 }
