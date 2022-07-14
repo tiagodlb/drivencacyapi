3bfa00eef50959a32e4d21a7f382647b2b5c7057
Tiago <tigolimab223@gmail.com>
2022-07-14 13:24:31 -0300

    fixed the routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import pollRouter from "../routes/pollRouter.js";
+import choiceRouter from "../routes/choiceRouter.js";
+import voteRouter from "../routes/voteRouter.js";
+import resultRouter from "../routes/resultRouter.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
+
+//Routes
+app.use(pollRouter);
+app.use(choiceRouter);
+app.use(voteRouter);
+app.use(resultRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(chalk.bold.green(`Server running on port ${process.env.PORT}`));
