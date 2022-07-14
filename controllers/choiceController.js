06c4c9e5476f4dc9510751a448a3fea17d0070eb
Tiago <tigolimab223@gmail.com>
2022-07-14 18:48:00 -0300

    bug fix

--- controllers/choiceController.js
+++ controllers/choiceController.js
@@ -10,7 +10,7 @@ export async function postChoice(req, res) {
   try {
     const pollExists = await db
       .collection("polls")
-      .findOne({ _id: ObjectId(poolId) });
+      .findOne({ _id: ObjectId(req.body.poolId) });
 
     if (!pollExists || pollExists === null) {
       return res.sendStatus(404);
