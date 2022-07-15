0c2646738d77fb11468b23583fad6207eaca3434
Tiago <tigolimab223@gmail.com>
2022-07-14 23:29:17 -0300

    bug fix

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -30,10 +30,12 @@ export async function postChoiceIdVote(req, res) {
   const id = req.params.id;
   let now = dayjs();
 
-  if(/[0-9a-fA-F]{24}/.test(id) === false) return res.sendStatus(404); //Checks if the value is an hexadecimal number
-  
+  if (/[0-9a-fA-F]{24}/.test(id) === false) return res.sendStatus(404); //Checks if the value is an hexadecimal number
+
   try {
-    let voteExists = await db.collection("choices").findOne({ _id: ObjectId(id) });
+    let voteExists = await db
+      .collection("choices")
+      .findOne({ _id: ObjectId(id) });
     if (!voteExists) {
       return res.sendStatus(404);
     }
@@ -41,20 +43,22 @@ export async function postChoiceIdVote(req, res) {
     const pollExists = await db
       .collection("polls")
       .findOne({ _id: ObjectId(voteExists.pollId) });
-    console.log(pollExists);
 
     const data = pollExists.expireAt;
+    console.log(data);
 
     if (now.diff(data, "day") >= 30) {
       return res.sendStatus(403);
     }
-    
-    let voteData = await db.collection("votes").findOne({choiceId: voteExists._id})
+
+    let voteData = await db
+      .collection("votes")
+      .findOne({ choiceId: voteExists._id });
     await db.collection("votes").insertOne({
       title: 1,
       pollId: voteExists.pollId,
       choiceId: voteExists._id,
-      createdAt: now.format("YYYY-MM-DD HH:mm:ss")
+      createdAt: now.format("YYYY-MM-DD HH:mm:ss"),
     });
     res.sendStatus(201);
   } catch (error) {
